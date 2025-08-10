94141dc1cf955397292c080e1c15fd4523f06c11
Syed-Talmin <saiyedsufyan7@gmail.com>
2025-08-10 16:35:05 +0530

    port log

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,7 +1,6 @@
 const app = require("./src/app");
 
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${port}`);
 });
